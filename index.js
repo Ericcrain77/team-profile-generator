58c6d4236420072cc4fa3a2fe6fefa9c1a91d405
Eric Crain <ericcrain77@gmail.com>
2021-10-25 14:49:08 -0400

    working on index.js file

--- index.js
+++ index.js
@@ -45,9 +45,9 @@ const employeeType = () => {
         let { role } = employeeTypeData;
 
         if (role === 'Engineer') {
-            engineerQuestions;
+            engineerQuestions();
         } else if (role === 'Intern') {
-            internQuestions;
+            internQuestions();
         } else {
             console.log('Failed to choose an employee type (employeeType() Function)');
         }
@@ -97,3 +97,109 @@ const internQuestions = () => {
         employeeQuestions(role, school);
     })
 };
+
+const employeeQuestions = (value1, value2) => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            messgae: "What is the employee's full name?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Enter Employee's Full Name.");
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'id',
+            message: "What is the employee's ID number?",
+            validate: nameInput => {
+                if (isNaN(nameInput)) {
+                    console.log("Enter employee's ID number.");
+                    return false;
+                } else {
+                    return true;
+                }
+            }
+        },
+        {
+            type: 'input',
+            name: 'email',
+            message: "What is the employee's email address?",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Enter employee's email address.");
+                }
+            }
+        }
+    ])
+    .then(employeeData => {
+        if (value1 === 'Manager') {
+            let officeNumber = value2;
+            let { name, id, email } = employeeData;
+            const manager = new Manager (name, id, email, officeNumber);
+            teamMembers.push(manager);
+        } else if (value1 === 'Engineer') {
+            let gitHubUsername = value2;
+            let { name, id, email } = employeeData;
+            const engineer = new Engineer (name, id, email, gitHubUsername);
+            teamMembers.push(engineer);
+        } else if (value1 === 'Intern') {
+            let school = value2;
+            let { name, id, email } = employeeData;
+            const intern = new Intern (name, id, email, school);
+            teamMembers.push(intern);
+        } else {
+            console.log('Failed to enter employee data correctly (employeeQuestions constructor function)')
+        }
+        addEmployee();
+    })
+};
+
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: 'Add another employee to the team?',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+    ])
+    .then(employeeAdd => {
+        let { addEmployee } = employeeAdd;
+        if (addEmployee === true) {
+            employeeType();
+        } else {
+            console.log('Your team is complete');
+            return writeToFile(teamMembers);
+        }
+    })
+};
+
+function writeToFile(teamMembers) {
+    const team = generatePage(teamMembers);
+    fs.writeFile('./dist/index.html', team, err => {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        console.log('Team page created!')
+    })
+};
+
+function init() {
+    managerQuestions();
+};
+
+init();
